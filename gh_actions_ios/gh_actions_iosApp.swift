7f0788f73c260838e79f52d9f6a571a8544fb718
Camilo SOT <cplaza@seidoropentrends.com>
2024-03-19 18:35:17 +0100

    Create ci_ios.yml (#1)
    * Create First ci_ios.yml
    * ADD iOS project
    * Use macos-14
    * ADD Unit Testing

---
+++ gh_actions_ios/gh_actions_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  gh_actions_iosApp.swift
+//  gh_actions_ios
+//
+//  Created by Camilo Plaza on 19/3/24.
+//
+
+import SwiftUI
+
+@main
+struct gh_actions_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
