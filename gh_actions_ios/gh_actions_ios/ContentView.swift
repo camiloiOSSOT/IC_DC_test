06d9f822ae85e1afa5210f1fd60e01e88583478e
Camilo Plaza <cplaza@opentrends.net>
2024-03-19 17:47:47 +0100

    ADD iOS project

---
+++ gh_actions_ios/gh_actions_ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gh_actions_ios
+//
+//  Created by Camilo Plaza on 19/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
